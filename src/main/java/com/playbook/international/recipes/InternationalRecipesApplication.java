41b4f26632b88e5c1980f4f290bd803da9ccadf8
Bihter Yalçın <76825689+bihter-yalcin@users.noreply.github.com>
2024-02-21 22:27:12 +0300

    Initial commit

---
+++ src/main/java/com/playbook/international/recipes/InternationalRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.playbook.international.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalRecipesApplication.class, args);
+	}
+
+}
