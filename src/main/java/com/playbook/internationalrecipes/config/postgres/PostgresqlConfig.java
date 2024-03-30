2b047f88b97ae305fc3f20d020c1b8ba5797eb33
Bihter Yalçın <76825689+bihter-yalcin@users.noreply.github.com>
2024-03-30 14:44:05 +0300

    FT-3 Add postgres configs

---
+++ src/main/java/com/playbook/internationalrecipes/config/postgres/PostgresqlConfig.java
@@ -0,0 +1,47 @@
+package com.playbook.internationalrecipes.config.postgres;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.postgresql.spring.datasource")
+public class PostgresqlConfig {
+
+    private String url;
+    private String username;
+    private String password;
+
+    @Autowired
+    public PostgresqlConfig(
+            @Value("${app.postgresql.url}") String url,
+            @Value("${app.postgresql.username}") String username,
+            @Value("${app.postgresql.password}") String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Define DataSource bean
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    // Define JdbcTemplate bean
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
